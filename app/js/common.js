3423936c32cf4ad6ebe9bf3f50f9d4bbba2b5ab3
AlexSlet <lekhasleta95@gmail.com>
2017-11-22 15:18:19 +0200

    Finish

--- app/js/common.js
+++ app/js/common.js
@@ -14,4 +14,52 @@
 	$('.close').click(function(){
 		$('.menu-burger').removeClass('is-active');
 	});
+
+	var mql = window.matchMedia('(max-width: 768px)');
+	if (mql.matches) {
+	    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+	} 
+	var mql = window.matchMedia('(max-width: 480px)');
+	if (mql.matches) {
+	    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+	} 
+	$(window).resize(function () {
+    var widthWind = $(this).width();
+
+    if (widthWind <= 750) {
+    	var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+    }
+    if (widthWind <= 480) {
+    	var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+    }
+  });
+
 });
